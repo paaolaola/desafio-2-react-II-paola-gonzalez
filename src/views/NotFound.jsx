947e681223b4ddd7f419fb69f4ff6988d785291c
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-11-12 13:36:55 -0300

    second commit

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 function NotFound() {
     return (
         <Container className="container-message">
             <h3 className="text-error">Error 404: Page not found ⚠️</h3>
+            <div>
+                <Link to="/" className="button-home">
+                    Back to Home
+                </Link>
+            </div>
         </Container>
     );
 }
